6ab6436b74a3f6bb12719ae8ff00e309f3b39364
nolanreyes <dylannolan72@gmail.com>
2024-04-12 17:58:38 +0100

    Backup before upgrade

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,6 @@
 const bool isProduction = false; // !!!! false for local, true for production !!!!
 const String productionUrl = 'https://dylannolan.com';
-const String localUrl = 'http://localhost:8000';
+//const String localUrl = 'http://localhost:8000';
+const String localUrl = 'https://surely-direct-jennet.ngrok-free.app';
 
 const String apiUrl = isProduction ? productionUrl : localUrl;
