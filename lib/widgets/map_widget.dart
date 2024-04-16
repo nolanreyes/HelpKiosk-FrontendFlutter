7191f27156cc1d2f55efe795505c1eaa6fb3f6db
nolanreyes <dylannolan72@gmail.com>
2024-04-16 02:10:24 +0100

    project fair

--- lib/widgets/map_widget.dart
+++ lib/widgets/map_widget.dart
@@ -4,6 +4,8 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:helpkiosk_frontend/controllers/locations_controller.dart';
 import 'package:helpkiosk_frontend/services/location_service.dart';
 import 'package:helpkiosk_frontend/models/locations.dart';
+import 'package:helpkiosk_frontend/constants.dart';
+import 'package:http/http.dart' as http;
 
 // Widget for displaying the map content
 class MapContent extends StatefulWidget {
@@ -15,6 +17,8 @@ class _MapContentState extends State<MapContent> {
   // Initialize the locations controller
   LocationsController locationsController = Get.put(LocationsController());
 
+  String message = '';
+
   @override
   void initState() {
     super.initState();
@@ -25,49 +29,92 @@ class _MapContentState extends State<MapContent> {
 
   // Handler for when a marker is tapped
   void handleMarkerTap(Location location) {
-    // Show a bottom sheet with location details and a button for getting directions
-    showModalBottomSheet(
+    // Show a dialog with location details and a button for getting directions
+    showDialog(
       context: context,
       builder: (BuildContext context) {
-        return Container(
-          height: 120,
-          padding: EdgeInsets.all(20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(location.resourceName,
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-              SizedBox(height: 10),
-              TextButton(
-                child: Text("Get Directions"),
-                onPressed: () async {
-                  LocationService locationService = LocationService();
-
-                  try {
-                    LatLng currentLocation =
-                        await locationService.getCurrentLocation();
-                    LatLng destinationLocation =
-                        LatLng(location.latitude, location.longitude);
-
-                    await locationService.getDirections(
-                        currentLocation, destinationLocation);
-
-                    Navigator.pop(context);
-                  } catch (error) {
-                    print("Error getting directions: $error");
-                  }
-                },
+        return StatefulBuilder(
+          builder: (BuildContext context, StateSetter setState) {
+            return Dialog(
+              child: Container(
+                height: MediaQuery.of(context).size.height * 0.5,
+                width: MediaQuery.of(context).size.width * 0.5,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0), // Add padding here
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    // Center the children vertically
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    // Center the children horizontally
+                    children: <Widget>[
+                      Text(location.resourceName,
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold)),
+                      SizedBox(height: 10),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text('Location Type: ${location.locationType}'),
+                      ),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text('Address: ${location.address}'),
+                      ),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text('Description: ${location.description}'),
+                      ),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text('Hours: ${location.hours}'),
+                      ),
+                      SizedBox(height: 10),
+                      TextButton(
+                        child: Text("Get Directions"),
+                        onPressed: () async {
+                          // implement code for directions
+                        },
+                      ),
+                      // Check if the location is a shelter
+                      if (location.locationType == 'SHELTER')
+                        TextButton(
+                          child: Text("Book Accommodation"),
+                          onPressed: () {
+                            allocateBedToGuest(setState);
+                          },
+                        ),
+                      Text(message), // Display the message here
+                    ],
+                  ),
+                ),
               ),
-            ],
-          ),
+            );
+          },
         );
       },
     );
   }
 
+  void allocateBedToGuest(StateSetter setState) async {
+    final response = await http
+        .get(Uri.parse('$apiUrl/sheltermanagement/allocate-bed-flutter/'));
+    if (response.statusCode == 200) {
+      setState(() {
+        message = 'Bed allocated successfully';
+      });
+    } else {
+      setState(() {
+        message = 'Failed to allocate bed to guest';
+      });
+    }
+    Future.delayed(Duration(seconds: 2), () {
+      setState(() {
+        message = '';
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    // Use Obx to reactively update the map when the locations change
     return Obx(() {
       if (locationsController.location.isNotEmpty) {
         return Stack(
